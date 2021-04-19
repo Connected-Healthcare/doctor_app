02b2972625e576e52aac439f94470b7886d63fdd
Niket Naidu <coder137@gmail.com>
2021-04-18 23:37:23 -0700

    Updated individual.dart to depend on IndividualChartScreen
    - main.dart initializes Firebase

--- doctor_app/lib/main.dart
+++ doctor_app/lib/main.dart
@@ -4,9 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [
